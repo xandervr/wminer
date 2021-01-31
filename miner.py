df85204c45c4edb30be7707a7d5606781c21aa50
Xander Van Raemdonck <xandervanraemdonck@gmail.com>
2021-01-31 18:55:12 +0100

    Added coinbase transaction + block size calculation

--- miner.py
+++ miner.py
@@ -1,6 +1,8 @@
 from hashlib import sha256
 import http.client
 import json
+import sys
+from models.block import Block
 from models.helpers import generateMerkleRoot
 from time import time
 
@@ -18,30 +20,60 @@ class Miner:
         self.version = ''
         self.previous_hash = ''
         self.difficulty = ''
+        self.block_size = 512000
+        self.block_reward = 0
+        self.miner_address = ''
         pass
 
-    def loadChainInfo(self, version: str, previous_hash: str, difficulty: int):
+    def loadChainInfo(self, version: str, previous_hash: str, difficulty: int, block_size: int, block_reward: int):
         self.version = version
         self.previous_hash = previous_hash
         self.difficulty = difficulty
+        self.block_size = block_size
+        self.block_reward = block_reward
 
     def generateBaseString(self, merkle_root):
         return self.version + littleEndian(self.previous_hash) + littleEndian(merkle_root) + littleEndian(
             hex(self.difficulty))
 
-    def mineBlock(self, transactions):
-        merkle_root = generateMerkleRoot(transactions)
+    def assembleBlock(self, transactions):
+        idx = 0
+        totalFee = 0
+        chosenTransactions = []
+        coinbaseTransaction = {
+            'timestamp': int(time()),
+            'sender': 'coinbase',
+            'receiver': self.miner_address,
+            'amount': 0,
+            'fee': 0,
+            'message': '',
+            'signature': '',
+            'pubkey': ''
+        }
+        self.block_size -= sys.getsizeof(coinbaseTransaction) + sys.getsizeof(float)
+        while idx < len(transactions) and self.block_size >= sys.getsizeof(coinbaseTransaction):
+            tx = transactions[idx]
+            totalFee += tx['fee']
+            self.block_size -= sys.getsizeof(tx)
+            chosenTransactions.append(tx)
+            idx += 1
+        coinbaseTransaction['amount'] = self.block_reward + totalFee
+        chosenTransactions.insert(0, coinbaseTransaction)
+        merkle_root = generateMerkleRoot(chosenTransactions)
         base_string = self.generateBaseString(merkle_root)
+        return (base_string, chosenTransactions)
+
+    def mineBlock(self, template: str, transactions):
         nonce = 1
         start = time()
         while nonce < MAX_NONCE:
-            hash = sha256(f"{base_string}{littleEndian(hex(nonce))}".encode('utf-8')).hexdigest()
+            hash = sha256(f"{template}{littleEndian(hex(nonce))}".encode('utf-8')).hexdigest()
             # print(hash)
             if int(hash, 16) < self.difficulty:
                 end = str(time() - start)
                 print(f"FOUND HASH: {hash} NONCE: {nonce} in {end} seconds")
                 block = {
-                    "transactions": list(map(lambda x: x['signature'], transactions)),
+                    "transactions": transactions,
                     "nonce": nonce
                 }
                 self.sendBlock(json.dumps(block))
@@ -80,6 +112,8 @@ class Miner:
             conn.request('POST', '/blocks', json, headers)
             response = conn.getresponse()
             conn.close()
+            print(response.status)
+            print(response.read().decode())
             if response.status == 200:
                 return True
             else:
@@ -91,9 +125,17 @@ class Miner:
     def startMiner(self):
         while True:
             chain_info = self.getChainInfo()
-            self.loadChainInfo(chain_info['version'], chain_info['previous_hash'], chain_info['difficulty'])
+            if chain_info is None:
+                break
+            self.loadChainInfo(
+                chain_info['version'],
+                chain_info['previous_hash'],
+                chain_info['difficulty'],
+                chain_info['block_size'],
+                chain_info['block_reward'])
             txs = m.getTransactions()
-            self.mineBlock(txs)
+            (template, chosenTxs) = self.assembleBlock(txs)
+            self.mineBlock(template, chosenTxs)
 
 
 m = Miner()
